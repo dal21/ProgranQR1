3187f803afecb45a1f5a78350cb3d89aa55a9b8b
dal21 <38189650+dal21@users.noreply.github.com>
2021-12-29 12:00:21 +0000

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/AR-Feb9.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/AR-Feb10.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
