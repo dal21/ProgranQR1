4012186b1fa0dd85548c28bbbd80c3f7700500c1
dal21 <38189650+dal21@users.noreply.github.com>
2022-11-01 16:32:50 +0000

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Christmas Progranimate4.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Christmas Progranimate6.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
